05f76b3b59ea7814587b18ef8bfc69c0d5566dcc
Ling Hung <ling@xappmedia.com>
2016-05-31 14:29:43 -0400

    Initial Commit

---
+++ Filterer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by jpk on 5/31/16.
+//  Copyright © 2016 Ling Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
