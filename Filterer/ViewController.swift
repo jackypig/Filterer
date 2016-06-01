7b79d4dd40795f875383f8abceec78b94f1f8327
Ling Hung <ling@xappmedia.com>
2016-06-01 14:25:56 -0400

    New photo, filter, and share functions

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -8,54 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var filteredImage: UIImage?
+    
     @IBOutlet var imageView: UIImageView!
     
-    @IBOutlet var imageToggle: UIButton!
+    @IBOutlet var secondaryMenu: UIView!
     
-    @IBAction func onImageToggle(sender: UIButton) {
-        imageView.image = filteredImage
-        if imageToggle.selected {
-            let image = UIImage(named: "Max")!
-            imageView.image = image
-            imageToggle.selected = false
-        } else {
-            imageView.image = filteredImage
-            imageToggle.selected = true
-        }
-    }
+    @IBOutlet var filterButton: UIButton!
+    
+    @IBOutlet var bottomMenu: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let image = UIImage(named: "Max")!
         
-        var rgbaImage = RGBAImage(image: image)!
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+        secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         
-        let avgRed = 107
-        
-        for y in 0..<rgbaImage.height {
-            for x in 0..<rgbaImage.width {
-                let index = y * rgbaImage.width + x
-                
-                var pixel = rgbaImage.pixels[index]
-                
-                let redDelta = Int(pixel.red) - avgRed
-                
-                var modifier = 1 + 4 * (Double(y) / Double (rgbaImage.height))
-                if (Int(pixel.red) < avgRed) {
-                    modifier = 1
-                }
-                
-                pixel.red = UInt8(max(min(255, Int(round(Double (avgRed) + modifier * Double(redDelta)))), 0))
-                rgbaImage.pixels[index] = pixel
-            }
-        }
-        
-        let result = rgbaImage.toUIImage()
-        filteredImage = result
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,6 +34,95 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onNewPhoto(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
 
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+            self.showCamera()
+            
+        }))
+
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: {
+            action in
+            self.showAlbum()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler:
+            nil))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func onShare(sender: AnyObject) {
+        let activityController = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        presentViewController(activityController, animated: true, completion: nil)
+    }
+    
+    func showCamera() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .Camera
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    func showAlbum() {
+        let albumPicker = UIImagePickerController()
+        albumPicker.delegate = self
+        albumPicker.sourceType = .PhotoLibrary
+        
+        presentViewController(albumPicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    }
+    
+    @IBAction func onFilter(sender: UIButton) {
+        if (sender.selected) {
+            hideSecondaryMenu()
+            sender.selected = false
+            
+        } else {
+            showSecondaryMenu()
+            sender.selected = true
+        }
+
+    }
+
+    func showSecondaryMenu() {
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+        
+        let leftConstraint = secondaryMenu.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        let heightConstraint = secondaryMenu.heightAnchor.constraintEqualToConstant(44)
+        
+        NSLayoutConstraint.activateConstraints([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+        UIView.animateWithDuration(0.4) {
+            self.secondaryMenu.alpha = 1.0
+        }
+    }
+    
+    func hideSecondaryMenu() {
+        UIView.animateWithDuration(0.4, animations: {self.secondaryMenu.alpha = 0
+            }) { completed in
+                if completed == true {
+                    self.secondaryMenu.removeFromSuperview()
+                }
+        }
+    }
 }
 
